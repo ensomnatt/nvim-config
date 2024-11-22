dc6690179167626093c091e4549b5bb1d346cc19
shushmyr <shushmyr@duck.com>
2024-11-22 17:28:48 +0300

    first commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function() require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "go"
+  		},
+  	},
+  },
+
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function ()
+      require("configs.lint")
+    end,
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit" }
+    }
+  },
+
+  {
+  'Exafunction/codeium.vim',
+  event = 'BufEnter'
+  },
+}
